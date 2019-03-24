62083b5e84ddcb16b1ed568437d2b83f316681b8
GauriBodkhe <gauribodkhe@gmail.com>
2019-03-24 00:18:57 -0500

    Update config.js

--- website/js/config.js
+++ website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_ABCDEFG', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1a2b3c4d5f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'us-west-2_mHIJDPix4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5puj50o0v7gb73lv3dqbeke3pu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://ee3r2rg9ze.execute-api.us-west-2.amazonaws.com/s1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
